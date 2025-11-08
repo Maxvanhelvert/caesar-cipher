ec6b8173da07c75593c2c2e6c77afd46c2e375d2
Maxvanhelvert <35576165+Maxvanhelvert@users.noreply.github.com>
2025-11-08 20:48:43 +1100

    basic ceasar cipher program

---
+++ ceasar-cipher.rb
@@ -0,0 +1,52 @@
+# a function that takes a string and a number to shift. 
+# separate the string into characters
+# convert the characters to their ascii code
+# apply the shift
+# check that the shift doesn't go past z -> if so, subtract 26 from the number. (blank spaces?)
+# convert numbers back to ascii characters
+# convert the array back to string. 
+# return the string
+# 
+# additional: ask for user input for string and shift number. 
+
+def upcase_ascii_transform(char, number)
+  ascii = char.ord + number
+  if ascii > 89
+    offset = ascii - 26
+    offset.chr
+  else 
+    ascii.chr
+  end
+end
+
+def downcase_ascii_transform(char, number)
+  ascii = char.ord + number
+  if ascii > 121 
+    offset = ascii - 26
+    offset.chr
+  else 
+    ascii.chr
+  end
+end
+
+def ceasar_cipher(string, number) 
+  arr = string.split('')
+  result = arr.map do |char| 
+    if char == " "
+      char
+    elsif char.upcase == char
+      upcase_ascii_transform(char, number)
+    else 
+      downcase_ascii_transform(char, number)
+    end
+  end
+  result.join
+end
+
+puts "Chose a word or sentence to encode:"
+user_string = gets.chomp
+puts "Chose an offset:"
+user_offset = gets.chomp.to_i
+
+puts "Your encoded word or sentence"
+puts ceasar_cipher(user_string, user_offset)
